7556da4f0fc76babeab2b2fa8c8267efe15705fa
Amer Almadani <amer@ma.tc>
2020-06-21 21:42:47 +0300

    ambience-aloe

--- source/unofficial-jolla-language-pack-ar/ambience-angrybirds/ambience-angrybirds.ts
+++ source/unofficial-jolla-language-pack-ar/ambience-angrybirds/ambience-angrybirds.ts
@@ -1,45 +1,45 @@
-<?xml version='1.0' encoding='utf-8'?>
+<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_GB">
+<TS version="2.0" language="ar">
 <context>
     <name></name>
     <message id="ambience-angrybirds-name">
-        <location filename="AmbienceTranslations.qml" line="6"></location>
+        <location filename="AmbienceTranslations.qml" line="6"/>
         <source>Angry Birds</source>
         <translation>Angry Birds</translation>
     </message>
     <message id="angrybirds-ringtone">
-        <location filename="AmbienceTranslations.qml" line="8"></location>
+        <location filename="AmbienceTranslations.qml" line="8"/>
         <source>Angry Birds Theme</source>
         <translation>Angry Birds Theme</translation>
     </message>
     <message id="angrybirds-imtone">
-        <location filename="AmbienceTranslations.qml" line="10"></location>
+        <location filename="AmbienceTranslations.qml" line="10"/>
         <source>Angry Birds IM</source>
         <translation>Angry Birds IM tone</translation>
     </message>
     <message id="angrybirds-chattone">
-        <location filename="AmbienceTranslations.qml" line="12"></location>
+        <location filename="AmbienceTranslations.qml" line="12"/>
         <source>Angry Birds Chat</source>
         <translation>Angry Birds chat tone</translation>
     </message>
     <message id="angrybirds-emailtone">
-        <location filename="AmbienceTranslations.qml" line="14"></location>
+        <location filename="AmbienceTranslations.qml" line="14"/>
         <source>Angry Birds Email</source>
         <translation>Angry Birds email tone</translation>
     </message>
     <message id="angrybirds-messagetone">
-        <location filename="AmbienceTranslations.qml" line="16"></location>
+        <location filename="AmbienceTranslations.qml" line="16"/>
         <source>Angry Birds Message</source>
         <translation>Angry Birds message tone</translation>
     </message>
     <message id="angrybirds-calendar-alarm">
-        <location filename="AmbienceTranslations.qml" line="18"></location>
+        <location filename="AmbienceTranslations.qml" line="18"/>
         <source>Angry Birds Calendar</source>
         <translation>Angry Birds Calendar alarm tone</translation>
     </message>
     <message id="angrybirds-clock-alarm">
-        <location filename="AmbienceTranslations.qml" line="20"></location>
+        <location filename="AmbienceTranslations.qml" line="20"/>
         <source>Angry Birds Clock</source>
         <translation>Angry Birds Clock alarm tone</translation>
     </message>
