7953e3c301d1fdf1d91011587d4c85fb962b27a7
Amer Almadani <amer@ma.tc>
2020-06-21 22:04:24 +0300

    Aloe and Settings

--- source/unofficial-jolla-language-pack-ar/jolla-settings/settings.ts
+++ source/unofficial-jolla-language-pack-ar/jolla-settings/settings.ts
@@ -1,113 +1,113 @@
-<?xml version='1.0' encoding='utf-8'?>
+<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_GB">
+<TS version="2.0" language="ar">
 <context>
     <name></name>
     <message id="settings-me-remove_from_favorites">
-        <location filename="../plugin/qml/FavoriteMenu.qml" line="15"></location>
+        <location filename="../plugin/qml/FavoriteMenu.qml" line="15"/>
         <source>Remove from Favorites</source>
-        <translation>Remove from favourites</translation>
+        <translation>إزالة من المفضلة</translation>
     </message>
     <message id="settings-me-add_to_favorites">
-        <location filename="../plugin/qml/FavoriteMenu.qml" line="17"></location>
+        <location filename="../plugin/qml/FavoriteMenu.qml" line="17"/>
         <source>Add to Favorites</source>
-        <translation>Add to favourites</translation>
+        <translation>إضافة إلى المفضلة</translation>
     </message>
     <message id="settings-la-in_progress">
-        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="22"></location>
+        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="22"/>
         <source>In progress</source>
-        <translation>In progress</translation>
+        <translation>قيد التنفيذ</translation>
     </message>
     <message id="settings-la-disabled_by_sailfish_device_manager">
-        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="27"></location>
+        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="27"/>
         <source>Disabled by %1 Device Manager</source>
         <extracomment>%1 is an operating system name without the OS suffix</extracomment>
-        <translation>Disabled by %1 Device Manager</translation>
+        <translation>تم  نعطيلها من قبل %1 إدارة الجهاز</translation>
     </message>
     <message id="settings-la-disabled_because_flight_mode_enabled">
-        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="32"></location>
+        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="32"/>
         <source>Disabled by flight mode</source>
-        <translation>Disabled by flight mode</translation>
+        <translation>تم تعطيلها من قبل وضع الطيران</translation>
     </message>
     <message id="settings-la-no-network-connectivity">
-        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="36"></location>
+        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="36"/>
         <source>No network connectivity</source>
-        <translation>No network connectivity</translation>
+        <translation>لا يوجد إتصال بالشبكة</translation>
     </message>
     <message id="settings-la-connection_failure">
-        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="40"></location>
+        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="40"/>
         <source>Connection failure</source>
-        <translation>Problem with connection</translation>
+        <translation>فشل الإتصال</translation>
     </message>
     <message id="settings-la-connection_setup_required">
-        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="44"></location>
+        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="44"/>
         <source>Connection setup required</source>
-        <translation>Connection setup required</translation>
+        <translation>إعدادات الإتصال مطلوبة</translation>
     </message>
     <message id="settings-la-no_sim_card_inserted">
-        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="48"></location>
+        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="48"/>
         <source>No SIM card inserted</source>
-        <translation>No SIM card inserted</translation>
+        <translation>الشريحة غير مدخلة</translation>
     </message>
     <message id="settings-la-sim_card_disabled">
-        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="52"></location>
+        <location filename="../plugin/qml/SettingsErrorNotification.qml" line="52"/>
         <source>SIM card disabled</source>
-        <translation>SIM card disabled</translation>
+        <translation>الشريحة معطلة</translation>
     </message>
     <message id="settings-me-go_to_settings">
-        <location filename="../plugin/qml/SettingsMenuItem.qml" line="7"></location>
+        <location filename="../plugin/qml/SettingsMenuItem.qml" line="7"/>
         <source>Go to settings</source>
         <extracomment>Show the relevant settings options for this.</extracomment>
-        <translation>Go to settings</translation>
+        <translation>إذهب إلى الإعدادات</translation>
     </message>
     <message id="settings-la-setting_off">
-        <location filename="../plugin/qml/SettingsToggle.qml" line="64"></location>
+        <location filename="../plugin/qml/SettingsToggle.qml" line="64"/>
         <source>Off</source>
         <extracomment>Shown when the setting is turned off</extracomment>
-        <translation>Off</translation>
+        <translation>مغلق</translation>
     </message>
     <message id="settings-la-setting_on">
-        <location filename="../plugin/qml/SettingsToggle.qml" line="67"></location>
+        <location filename="../plugin/qml/SettingsToggle.qml" line="67"/>
         <source>On</source>
         <extracomment>Shown when the setting is turned on</extracomment>
-        <translation>On</translation>
+        <translation>مُفعل</translation>
     </message>
     <message id="settings-system_settings">
-        <location filename="../src/dummytranslations/translations.qml" line="7"></location>
+        <location filename="../src/dummytranslations/translations.qml" line="7"/>
         <source>System settings</source>
-        <translation>System</translation>
+        <translation>إعدادات النظام</translation>
     </message>
     <message id="settings-applications">
-        <location filename="../src/dummytranslations/translations.qml" line="9"></location>
+        <location filename="../src/dummytranslations/translations.qml" line="9"/>
         <source>Applications</source>
-        <translation>Apps</translation>
+        <translation>تطبيق</translation>
     </message>
     <message id="settings-he-applications">
-        <location filename="../src/pages/ApplicationsGrid.qml" line="48"></location>
-        <location filename="../src/pages/ApplicationsPage.qml" line="20"></location>
+        <location filename="../src/pages/ApplicationsGrid.qml" line="48"/>
+        <location filename="../src/pages/ApplicationsPage.qml" line="20"/>
         <source>Applications</source>
-        <translation>Apps</translation>
+        <translation>تطبيق</translation>
     </message>
     <message id="settings-la-super_app">
-        <location filename="../src/pages/ApplicationsPage.qml" line="85"></location>
+        <location filename="../src/pages/ApplicationsPage.qml" line="85"/>
         <source>Super app</source>
-        <translation>Super app</translation>
+        <translation>تطبيق رهيب</translation>
     </message>
     <message id="settings-he-settings">
-        <location filename="../src/pages/FrontPage.qml" line="7"></location>
+        <location filename="../src/pages/FrontPage.qml" line="7"/>
         <source>Settings</source>
-        <translation>Settings</translation>
+        <translation>الإعدادات</translation>
     </message>
     <message id="settings-he-warning_settings_busy">
-        <location filename="../src/settings.qml" line="52"></location>
+        <location filename="../src/settings.qml" line="52"/>
         <source>Settings application is busy</source>
-        <extracomment>system notification shown when settings is requested to show a page but it's busy</extracomment>
-        <translation>Settings app is busy</translation>
+        <extracomment>system notification shown when settings is requested to show a page but it&apos;s busy</extracomment>
+        <translation>تطبيق الإعدادات مشغول</translation>
     </message>
     <message id="settings-ap-name">
-        <location filename="../src/main.cpp" line="41"></location>
+        <location filename="../src/main.cpp" line="41"/>
         <source>Settings</source>
-        <translation>Settings</translation>
+        <translation>الإعدادات</translation>
     </message>
 </context>
 </TS>
