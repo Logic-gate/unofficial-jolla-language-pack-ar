ba555fe21afba4e51531fc2ebc83fa0dc16f6b97
Amer Almadani <mad_dev@linuxmail.org>
2020-06-30 14:43:00 +0300

    Jolla-devicelock/devicelock.ts
    #34

--- source/ar_SA/unofficial-jolla-language-pack-ar/jolla-devicelock/devicelock.ts
+++ source/ar_SA/unofficial-jolla-language-pack-ar/jolla-devicelock/devicelock.ts
@@ -1,46 +1,52 @@
-<?xml version='1.0' encoding='utf-8'?>
+<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_GB">
+<TS version="2.1" language="ar_SA">
 <context>
     <name></name>
     <message id="devicelock-he-locked">
-        <location filename="qml/main.qml" line="21"></location>
+        <location filename="qml/main.qml" line="21"/>
         <source>Locked</source>
-        <extracomment>Devicelock UI's header-text which indicates Locked state.</extracomment>
-        <translation>Locked</translation>
+        <extracomment>Devicelock UI&apos;s header-text which indicates Locked state.</extracomment>
+        <translation>مُغلق</translation>
     </message>
     <message id="devicelock-bt-unlock">
-        <location filename="qml/main.qml" line="25"></location>
+        <location filename="qml/main.qml" line="25"/>
         <source>Unlock</source>
-        <extracomment>Devicelock UI's unlock-key which is pressed to confirm the lockcode.</extracomment>
-        <translation>Unlock</translation>
+        <extracomment>Devicelock UI&apos;s unlock-key which is pressed to confirm the lockcode.</extracomment>
+        <translation>فتح الجهاز</translation>
     </message>
     <message id="settings_devicelock-la-devicelock_max_length_warning" numerus="yes">
-        <location filename="qml/main.qml" line="28"></location>
+        <location filename="qml/main.qml" line="28"/>
         <source>Lock code cannot be more than %n digits.</source>
-        <translation><numerusform>Security code cannot be longer that %n digit</numerusform>
-        <numerusform>Security code cannot be longer that %n digits</numerusform>
-        </translation></message>
+        <translation>
+            <numerusform>لا يمكن أن يكون رمز الأمان أطول من٪n</numerusform>
+            <numerusform>لا يمكن أن يكون رمز الأمان أطول من٪ n</numerusform>
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
     <message id="devicelock-he-permanently_locked">
-        <location filename="qml/main.qml" line="45"></location>
-        <location filename="qml/main.qml" line="68"></location>
+        <location filename="qml/main.qml" line="45"/>
+        <location filename="qml/main.qml" line="68"/>
         <source>Permanently Locked</source>
-        <translation>Permanently locked</translation>
+        <translation>مقفل بشكل دائم</translation>
     </message>
     <message id="devicelock-he-final_chance">
-        <location filename="qml/main.qml" line="50"></location>
+        <location filename="qml/main.qml" line="50"/>
         <source>Final chance</source>
-        <translation>Final try</translation>
+        <translation>المحاولة الأخيرة</translation>
     </message>
     <message id="devicelock-la-devicelock_last_chance">
-        <location filename="qml/main.qml" line="53"></location>
+        <location filename="qml/main.qml" line="53"/>
         <source>Entering incorrent lock code once more will permanently block your access to the device.</source>
-        <translation>If you enter an incorrect security code once more, your device will be permanently locked</translation>
+        <translation>إن أدخلت رمز أمان غير صحيح مرة أخرى سيتم قفل جهازك بشكل دائم.</translation>
     </message>
     <message id="devicelock-la-devicelock_incorrect_lock_code">
-        <location filename="qml/main.qml" line="58"></location>
+        <location filename="qml/main.qml" line="58"/>
         <source>Incorrect lock code</source>
-        <translation>Incorrect security code</translation>
+        <translation>رمز الأمان غير صحيح</translation>
     </message>
 </context>
 </TS>
