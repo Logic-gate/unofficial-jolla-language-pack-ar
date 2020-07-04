aeabbb641879da0ef763b975e4987a1815bc3130
Amer Almadani <mad_dev@linuxmail.org>
2020-07-04 14:06:13 +0300

    #42

--- source/ar_SA/unofficial-jolla-language-pack-ar/jolla-keyboard/jolla-keyboard.ts
+++ source/ar_SA/unofficial-jolla-language-pack-ar/jolla-keyboard/jolla-keyboard.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_GB">
+<TS version="2.1" language="ar_SA">
 <context>
     <name></name>
     <message id="text_input-la-swipe_close_gesture_hint">
